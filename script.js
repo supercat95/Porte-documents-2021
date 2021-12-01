cbdde01a3684a9fab9d460969b6225e4010fb063
supercat95 <33792376+supercat95@users.noreply.github.com>
2021-12-01 04:40:27 +0000

    Remove autoplay for enviro iframe; Rename y variable and make global; Chain variables into embedCodeVideos.
    Issues: embedCodeVideos() does not return errors, but does not seem to work, either.

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 let widthOfTV;
-let yPosOfTV;
+let yValueOfTV;
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
     background(255,0,0);
 
     widthOfTV = windowWidth/8.0;
-    yPosOfTV = windowHeight/6.0;
+    yValueOfTV = windowHeight/6.0;
 }
 
 function draw() {
@@ -23,7 +23,7 @@ function drawTVsForCodeVideos() {
     for (let i = 0; i < 4; i++) {
         strokeWeight(5);
 
-        rect(xVal, yPosOfTV, windowWidth/6.0, windowHeight/6.0);
+        rect(xVal, yValueOfTV, windowWidth/6.0, yValueOfTV);
 
         drawSupportsForTVs(xVal);
         embedCodeVideos(i);
@@ -37,12 +37,12 @@ function drawSupportsForTVs(xVal) {
 
     strokeWeight(3);
 
-    line(x1, yPosOfTV, x1, 0);
-    line(x2, yPosOfTV, x2, 0);
+    line(x1, yValueOfTV / 2.0, x1, 0);
+    line(x2, yValueOfTV / 2.0, x2, 0);
 }
 
 function embedCodeVideos(i) {
     if (i == 0) { 
-        // document.getElementById("enviro").innerHTML = '<iframe> width="'widthOfTV'" height="'windowHeight/6.0'" src="https://www.youtube.com/embed/wCTVcycdYDg" frameborder="0" allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+        document.getElementById("enviro").innerHTML = '<iframe> width="' + widthOfTV + '" height="' + yValueOfTV + '" src="https://www.youtube.com/embed/wCTVcycdYDg" frameborder="0" allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
     }
 }
