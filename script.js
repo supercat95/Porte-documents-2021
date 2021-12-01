56368fc1a0bb79f91230a83d79761c0f1b8c9752
supercat95 <33792376+supercat95@users.noreply.github.com>
2021-12-01 04:29:49 +0000

    Make local TV variables global

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
+let widthOfTV;
+let yPosOfTV;
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
     background(255,0,0);
+
+    widthOfTV = windowWidth/8.0;
+    yPosOfTV = windowHeight/6.0;
 }
 
 function draw() {
@@ -13,33 +18,31 @@ function draw() {
 
 // --------------- TVs -------------------------------
 function drawTVsForCodeVideos() {
-    let widthOfTV = windowWidth/8.0;
-    let yVal = windowHeight/6.0;
     let xVal = widthOfTV;
     
     for (let i = 0; i < 4; i++) {
         strokeWeight(5);
 
-        rect(xVal, yVal, windowWidth/6.0, windowHeight/6.0);
+        rect(xVal, yPosOfTV, windowWidth/6.0, windowHeight/6.0);
 
-        drawSupportsForTVs(widthOfTV,xVal,yVal);
+        drawSupportsForTVs(xVal);
         embedCodeVideos(i);
         xVal+=(widthOfTV*2);
     }
 }
 
-function drawSupportsForTVs(widthOfTV, xVal, yVal) {
+function drawSupportsForTVs(xVal) {
     x1 = xVal - (widthOfTV / 2.0);
     x2 = xVal + (widthOfTV / 2.0);
 
     strokeWeight(3);
 
-    line(x1, yVal, x1, 0);
-    line(x2, yVal, x2, 0);
+    line(x1, yPosOfTV, x1, 0);
+    line(x2, yPosOfTV, x2, 0);
 }
 
 function embedCodeVideos(i) {
     if (i == 0) { 
-        document.getElementById("enviro").innerHTML = '<iframe> width="'widthOfTV'" height="'windowHeight/6.0'" src="https://www.youtube.com/embed/wCTVcycdYDg" frameborder="0" allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+        // document.getElementById("enviro").innerHTML = '<iframe> width="'widthOfTV'" height="'windowHeight/6.0'" src="https://www.youtube.com/embed/wCTVcycdYDg" frameborder="0" allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
     }
 }
