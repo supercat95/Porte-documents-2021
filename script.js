336902291eba37c3027fdc5329ae1989670d9bea
supercat95 <33792376+supercat95@users.noreply.github.com>
2021-12-03 21:21:25 +0000

    Resize and reposition TVs; Extend height of spotlights; Change some comments

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 // declaration of variables for code TVs
 let widthOfTV;
-let yValueOfTV;
-let xValOfTV;
+let heightOfTV;
+let xPosOfTV;
+let yPosOfTV;
+
+let strokeOfTV;
+let strokeOfSupports;
+let strokeDif;
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
@@ -9,14 +14,19 @@ function setup() {
 
     // assignment of variables for code TVs
     widthOfTV = windowWidth/5.0; // this is the radius
-    yValueOfTV = windowHeight/6.0;
-    xValOfTV = (widthOfTV * 1.2) - (widthOfTV / 1.6); // 1.2 2.0
+    heightOfTV = widthOfTV*.56;
+    xPosOfTV = (widthOfTV * 1.2) - (widthOfTV / 1.6); // 1.2 2.0
+    yPosOfTV = heightOfTV*.75;
+
+    strokeOfTV = 5;
+    strokeOfSupports = 3;
+    strokeDif = strokeOfTV - strokeOfSupports;
 }
 
 function draw() {
     // console.log(pmouseX + ' ' + pmouseY);  
     noLoop();
-    
+
     drawArches();
     drawSpotlightsUnderTVs();
     drawTVsForCodeVideos(); // call last because of error
@@ -31,28 +41,29 @@ function drawTVsForCodeVideos() {
     
     // spaces out the rects and lines for the TVs (horizontal)
     for (let i = 0; i < 4; i++) {
-        strokeWeight(5);
+        strokeWeight(strokeOfTV);
 
-        rect(xValOfTV, yValueOfTV, widthOfTV, yValueOfTV);
+        rect(xPosOfTV, yPosOfTV, widthOfTV, heightOfTV);
 
         drawSupportsForTVs();
         embedCodeVideos(i);
-        xValOfTV+=(widthOfTV * 1.09); // 1.2x based off widthOfTV/5.0 to equal 100% of screen width
-        if (i==1) { xValOfTV += widthOfTV/1.8; }
+
+        xPosOfTV+=(widthOfTV * 1.09); // 1.2x based off widthOfTV/5.0 to equal 100% of screen width
+        if (i==1) { xPosOfTV += widthOfTV/1.8; } // creates a gap in the middle of the screen
     }
 }
 
 // draws two verticle lines for each of the TV rectangles
 function drawSupportsForTVs() {
     // the widths are halved because rectMode is center
-    let x1 = xValOfTV - (widthOfTV / 2.0);
-    let x2 = xValOfTV + (widthOfTV / 2.0);
+    let x1 = xPosOfTV - (widthOfTV / 2.0);
+    let x2 = xPosOfTV + (widthOfTV / 2.0);
 
-    strokeWeight(3);
+    strokeWeight(strokeOfSupports);
 
-    // draws 2 lines from center of rect border to top of window. +- values are based on the difference of the stroke weights between the rects and the lines
-    line(x1 +1 , yValueOfTV / 2.0, x1 +1, 0);
-    line(x2 -1, yValueOfTV / 2.0, x2 -1, 0);
+    // draws 2 lines from center of rect border to top of window. +- values account for stroke values
+    line(x1 + strokeDif, yPosOfTV / 2.0, x1 + strokeDif/2, 0);
+    line(x2 - strokeDif, yPosOfTV / 2.0, x2 - strokeDif/2, 0);
 }
 
 // align and embed YT videos into the rects
@@ -65,11 +76,10 @@ function embedCodeVideos(i) {
     else if (i==2) { video = "galaxy"; }
     // else if (i==3) { video = "?"; } // UPDATE LATER
 
-    // the +- values are related to the strokeWeight of the rects
-    document.getElementById(video).width = widthOfTV - 5;
-    document.getElementById(video).height = yValueOfTV - 5;
-    document.getElementById(video).style.left = xValOfTV - (widthOfTV / 2.2) - 2.5 + 'px';
-    document.getElementById(video).style.top = yValueOfTV / 2.0 + 10 + 'px';
+    document.getElementById(video).width = widthOfTV - strokeOfTV;
+    document.getElementById(video).height = heightOfTV - strokeOfTV;
+    document.getElementById(video).style.left = xPosOfTV - (widthOfTV / 2.2) - (strokeOfTV / 2) + 'px';
+    document.getElementById(video).style.top = (yPosOfTV / 2.0) - (strokeOfTV * 2) + 'px';
 }
 
 // --------------- arches ----------------------------
@@ -106,6 +116,7 @@ function drawArches() {
     }
 }
 
+// draws 2 spotlights between the arches
 function drawSpotlightsUnderTVs() {
     let x1 = windowWidth/3; //2.6 
     let x2 = 2*windowWidth/3; //1.6
@@ -119,8 +130,8 @@ function drawSpotlightsUnderTVs() {
     ellipse(x2, y1, rad);
 
     // bottom, left, right
-    triangle(x1,y1, x1-rad,y1-yValueOfTV, x1+rad,y1-yValueOfTV);
-    triangle(x2,y1, x2-rad,y1-yValueOfTV, x2+rad,y1-yValueOfTV);
+    triangle(x1,y1, x1-rad,y1-heightOfTV, x1+rad,y1-heightOfTV);
+    triangle(x2,y1, x2-rad,y1-heightOfTV, x2+rad,y1-heightOfTV);
 }
 
   
